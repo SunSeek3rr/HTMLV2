a0858f90c9cbb1618d6f0f4cecba21fbb00957e7
SunSeek3rr <sebastien.dagostin@student.heaj.be>
2026-04-22 13:50:42 +0200

    Initial commit
    Custom cursor and imports (GSAP)

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const cursor = document.querySelector('.customCursor');
+
+gsap.set(".customCursor", { xPercent: -50, yPercent: -50});
+
+const colors = ['red', 'blue', 'yellow', 'pink'];
+let i = 0;
+
+document.addEventListener('mousemove', (e) =>{
+    gsap.to(cursor, {
+        x: e.clientX,
+        y: e.clientY,
+        duration: .24,
+        ease: "power1.out",
+    });
+
+});
+
+let previousTime = 0;
+
+function animate(timestamp) {
+    setTimeout(()=>{
+        i++;
+
+        gsap.to(cursor, {
+            backgroundColor: colors[i % 4],
+            duration: .24,
+            ease: "power2.out",
+        });
+
+        window.requestAnimationFrame( ( timestamp ) => {
+            animate( timestamp );
+        } );
+
+    }, 82);
+
+}
+
+window.requestAnimationFrame( ( timestamp ) => {
+    animate( timestamp );
+} );
